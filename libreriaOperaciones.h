4dba081f3a71414383ef32d99bc79e71ad4d4cba
Franco Albornoz <albfrank12@gmail.com>
2021-07-29 16:23:24 -0300

    Ejercicio1 y Ejercicio2 resueltos, agrego libreria.

---
+++ libreriaOperaciones.h
@@ -0,0 +1,14 @@
+
+int Sumar(int &a, int &b){
+	return (a+b);
+}
+int Restar(int &a, int &b){
+	return (a-b);
+}
+int  Producto (int &a,int &b){
+	return (a*b);
+}
+float Dividir(float &div1, float &div2){
+	return (div1/div2);
+}
+
